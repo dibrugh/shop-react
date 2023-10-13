433a72ae4c33c08e32a18ded2913484848064dae
dibrugh <dibrugh@gmail.com>
2023-10-13 20:14:26 +0300

    feat: create draft of shopping cart

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FaShoppingCart } from "react-icons/fa";
 
 export default function Header() {
+    let [cartOpen, setCartOpen] = useState(false);
+
     return (
         <header>
             {/* logo */}
@@ -11,6 +14,13 @@ export default function Header() {
                     <li>Контакты</li>
                     <li>Кабинет</li>
                 </ul>
+                <FaShoppingCart className={`shop-cart-button ${cartOpen && 'active'}`} onClick={() => setCartOpen(cartOpen = !cartOpen)} />
+
+                {cartOpen && (
+                    <div className='shop-cart'>
+                        
+                    </div>
+                )}
             </div>
             {/* banner */}
             <div className='presentation'></div>
